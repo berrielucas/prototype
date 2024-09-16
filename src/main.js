52b01bd32d6b647146577fca07d4b777a266561e
Lucas Berriel <mberrielucas@gmail.com>
2024-09-16 19:26:49 -0300

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// Config Vue and Router
+import App from "./App.vue";
+import router from "./router";
+import { createApp } from "vue";
+
+// Config Vuetify
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+// Config Pinia
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+// Config App
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.use(vuetify);
+app.mount("#app");
